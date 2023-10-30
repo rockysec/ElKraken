8b16cd417ffe5590e4b7a300b884e1a4935730f6
Efrain Reyes <48323046+rockysec@users.noreply.github.com>
2023-10-30 16:58:59 -0300

    Update install.sh

--- Tools/install.sh
+++ Tools/install.sh
@@ -18,19 +18,14 @@ sudo apt-get install -y xargs
 sudo apt-get install -y chromium chromium-l10n
 sudo apt-get install -y golang
 apt install -y libpcap-dev
-apt install -y whatweb
-apt install -y awscli
-apt install -y snap
 apt install -y tmux
 apt install -y dnsutils
 apt install -y curl
 apt-get install -y nmap
-snap install chromium
 pip3 install dirsearch
 
 pip install colored
 pip3 install colored
-pip install shodan
 pip3 install uro
 pip3 install requests
 
@@ -41,13 +36,6 @@ git clone https://github.com/projectdiscovery/nuclei-templates
 nuclei -update-templates
 
 cd ~/tools
-git clone https://github.com/rockysec/ElKraken.git
-chmod +x ~/tools/ElKraken/ElKrakenTool.sh
-
-git clone https://github.com/nahamsec/JSParser.git
-cd JSParser*
-sudo python setup.py install
-
 git clone https://github.com/udhos/update-golang
 cd update-golang
 sudo ./update-golang.sh
@@ -58,42 +46,10 @@ cd Sublist3r*
 pip install -r requirements.txt
 
 cd ~/tools/
-git clone https://github.com/m4ll0k/SecretFinder.git secretfinder
-cd secretfinder
-pip3 install -r requirements.txt
-python3 SecretFinder.py
-
-cd ~/tools/
-git clone https://github.com/1ndianl33t/Gf-Patterns
-
-cd ~/tools/
 git clone https://github.com/maurosoria/dirsearch.git
 
 cd ~/tools/
-git clone https://github.com/jobertabma/virtual-host-discovery.git
-
-cd ~/tools/
-git clone https://github.com/guelfoweb/knock.git
-
-cd ~/tools/
 git clone https://github.com/rockysec/customscripts
-
-cd ~/tools/
-git clone https://github.com/blechschmidt/massdns.git
-cd ~/tools/massdns
-make
-
-cd ~/tools/
-git clone https://github.com/nahamsec/crtndstry.git
-
-cd ~/tools/
-git clone https://github.com/0xbharath/assets-from-spf
-
-cd ~/tools/
-git clone https://github.com/internetwache/GitTools
-
-cd ~/tools/
-git clone https://github.com/gwen001/github-search
 
 cd ~/tools/
 git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git
@@ -110,11 +66,6 @@ cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
 curl -L -O https://github.com/projectdiscovery/httpx/releases/download/v1.0.3/httpx_1.0.3_linux_amd64.tar.gz
 tar -xzvf httpx_1.0.3_linux_amd64.tar.gz
 mv httpx /usr/local/bin/
- 
-cd ~/tools/
-curl -L -O https://github.com/lc/gau/releases/download/v1.1.0/gau_1.1.0_linux_amd64.tar.gz
-tar xvf gau_1.1.0_linux_amd64.tar.gz
-mv gau /usr/bin/gau
 
 cd ~/tools/
 curl -L -O https://github.com/projectdiscovery/nuclei/releases/download/v2.5.4/nuclei_2.5.4_linux_amd64.zip
@@ -123,11 +74,6 @@ mv nuclei /usr/bin/
 git clone https://github.com/projectdiscovery/nuclei-templates
 nuclei -update
 
-cd ~/tools/
-curl -L -O https://github.com/ffuf/ffuf/releases/download/v1.2.1/ffuf_1.2.1_linux_amd64.tar.gz
-tar -xzvf ffuf_1.2.1_linux_amd64.tar.gz
-mv ffuf /usr/bin/
-
 cd ~/tools
 git clone https://github.com/dwisiswant0/crlfuzz.git
 cd crlfuzz
@@ -135,40 +81,19 @@ go build cmd/crlfuzz/main.go
 mv main crlfuzz
 mv crlfuzz /usr/local/bin/
 
-cd ~/tools 
-git clone https://github.com/GerbenJavado/LinkFinder.git
-cd LinkFinder
-python setup.py install
-pip3 install -r requirements.txt
-
 curl -L -O https://github.com/projectdiscovery/subfinder/releases/download/v2.4.5/subfinder_2.4.5_linux_386.tar.gz
 tar -xzvf subfinder_2.4.5_linux_386.tar.gz
 cp subfinder /usr/local/bin/
-
-curl -L -O https://github.com/projectdiscovery/chaos-client/releases/download/v0.1.7/chaos-client_0.1.7_linux_amd64.tar.gz
-tar -xzvf chaos-client_0.1.7_linux_amd64.tar.gz 
-mv chaos /usr/bin/
-cd
 
 git clone https://github.com/1ndianl33t/Gf-Patterns
 mkdir .gf
 mv ~/Gf-Patterns/*.json ~/.gf
 
-go get -u github.com/ffuf/ffuf
-go get github.com/michenriksen/aquatone
-go get -u github.com/tomnomnom/unfurl 
-go get -v github.com/projectdiscovery/naabu/cmd/naabu
-go get -u -v github.com/projectdiscovery/shuffledns/cmd/shuffledns
-go get -u -v github.com/projectdiscovery/dnsprobe
-go get -u github.com/tomnomnom/anew
-go get -u github.com/tomnomnom/gf
-go get -u github.com/tomnomnom/qsreplace
-go get -u github.com/jaeles-project/gospider
-go get -v github.com/projectdiscovery/httpx/cmd/httpx
-go get github.com/tomnomnom/waybackurls
-go get -u github.com/tomnomnom/anew
-go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
-go install github.com/detectify/page-fetch@latest
+go install github.com/tomnomnom/anew@latest
+go install github.com/tomnomnom/gf@latest
+go install github.com/tomnomnom/qsreplace@latest
+go install github.com/projectdiscovery/httpx@latest
+go install github.com/tomnomnom/waybackurls@latest
 nuclei -update-templates
 cd ~/go/bin
 cp * /usr/bin/
